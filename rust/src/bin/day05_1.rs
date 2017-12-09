23bc5a1c4942671c031a560cfcbd8476c519885c
Wesley Ellis <tahnok@gmail.com>
2017-12-09 13:06:39 -0500

    day 5 part 1

---
+++ rust/src/bin/day05_1.rs
@@ -0,0 +1,28 @@
+use std::io;
+use std::io::Read;
+
+fn main() {
+    let mut input = String::new();
+    let _ = io::stdin().read_to_string(&mut input);
+    println!("{}", steps_to_escape(input.trim()));
+}
+
+fn steps_to_escape(input: &str) -> u32 {
+    let mut digits: Vec<i32> = input.lines().map(|x| x.parse().unwrap()).collect();
+    let mut index = 0;
+    let mut count = 0;
+    loop {
+        count = count + 1;
+
+        let jmp = digits[index];
+        let new_index = (index as i32 + jmp) as usize;
+        if new_index < digits.len() {
+            digits[index] = jmp + 1;
+            index = new_index;
+        } else {
+            break;
+        }
+    }
+    count
+}
+
