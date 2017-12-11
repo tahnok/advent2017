dc9f158eb3284ba02a49754c7a9916dd76042ec9
Wesley Ellis <wesley.ellis@shopify.com>
2017-12-11 16:55:46 -0500

    day 10 part 1

--- rust/src/bin/day10_1.rs
+++ rust/src/bin/day10_1.rs
@@ -1,3 +1,5 @@
+#![feature(slice_rotate)]
+
 use std::io;
 use std::io::Read;
 
@@ -12,20 +14,29 @@ fn hash(input: &str) -> usize {
     let lengths: Vec<usize> = input.split(",").map(|x| x.parse().unwrap()).collect();
 
     let mut position = 0;
-    let mut rope: Vec<usize> = (0..5).collect();
+    let mut rope: Vec<usize> = (0..256).collect();
     println!("{:?}", rope);
-    let mut skip_size = 1;
+    let rope_length = rope.len();
+
+    let mut skip_size = 0;
 
     for length in lengths {
+        println!("pos: {}", position);
         let mut front: Vec<usize> = rope.iter().cycle().skip(position).take(length).map(|x| *x).collect();
         front.reverse();
         println!("front: {:?}", front);
-        let mut back: Vec<usize> = rope.iter().cycle().skip(position + length).take(rope.len() - length).map(|x| *x).collect();
+        let mut back: Vec<usize> = rope.iter().cycle().skip(position + length).take(rope_length - length).map(|x| *x).collect();
         println!("back: {:?}", back);
+
         front.append(&mut back);
+        println!("before {:?}", front);
+        let rotate = rope_length - position;
+        println!("rotate {}", rotate);
+        front.rotate(rotate);
+        println!("after {:?}", front);
         rope = front;
-        println!("{:?}", rope);
-        position = length + skip_size;
+        position = (position + length + skip_size) % rope_length;
+        println!("------------");
 
         skip_size += 1;
     }
