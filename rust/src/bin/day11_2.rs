ed0cb53604ad9fdf7058da1bf4c5afbe6690121d
Wesley Ellis <tahnok@gmail.com>
2017-12-11 21:42:42 -0500

    day 11 part 2

---
+++ rust/src/bin/day11_2.rs
@@ -0,0 +1,54 @@
+use std::io;
+use std::io::Read;
+
+fn main() {
+    let mut input = String::new();
+    let _ = io::stdin().read_to_string(&mut input);
+    println!("{}", steps(input.trim()));
+}
+
+fn steps(input: &str) -> isize {
+    //https://www.redblobgames.com/grids/hexagons/
+    let mut x: isize = 0;
+    let mut y: isize = 0;
+    let mut z: isize = 0;
+
+    let mut max = 0;
+
+    for dir in input.split(",") {
+        match dir {
+            "n" => {
+                y += 1;
+                z -= 1;
+            },
+            "s" => {
+                y -= 1;
+                z += 1;
+            },
+            "ne" => {
+                x += 1;
+                z -= 1;
+            },
+            "nw" => {
+                x -= 1;
+                y += 1;
+            },
+            "sw" => {
+                x -= 1;
+                z += 1;
+            },
+            "se" => {
+                x += 1;
+                y -= 1;
+            },
+            _ => panic!("unexpected direction")
+        }
+        //println!("moved {} to {},{},{}",dir, x,y,z);
+        let new_max = (x.abs() + y.abs() + z.abs()) / 2;
+        if new_max > max {
+            max = new_max;
+        }
+    }
+    max
+}
+
