765ab74cc0e64603f866324e6a4d0a75fd67105c
Wesley Ellis <tahnok@gmail.com>
2017-12-15 22:18:32 -0500

    WIP day 7 part 2

--- rust/src/bin/day07_2.rs
+++ rust/src/bin/day07_2.rs
@@ -31,5 +31,51 @@ fn bottom_tower(input: &str) -> String {
             leaves.insert(name);
         }
     }
+
+    let root = "hlhomy";
+    check_weight(&root, &children_by_name, &weights);
+
     panic!("not found");
+}
+
+fn check_weight(node: &str, children_by_name: &HashMap<&str, Vec<&str>>, weights: &HashMap<&str, usize>) -> usize {
+    let weight = *weights.get(node).unwrap();
+    if !children_by_name.contains_key(node) {
+        return weight;
+    }
+    let children = children_by_name.get(node).unwrap();
+    
+
+    let left = check_weight(children[0], children_by_name, weights);
+    let middle = check_weight(children[1], children_by_name, weights);
+    let right = check_weight(children[2], children_by_name, weights);
+
+    if left != middle && middle == right {
+        let weight = weights.get(children[0]).unwrap();
+        if left > middle {
+            panic!("{} should be {}", weight, (weight - (left - middle)));
+        } else {
+            panic!("{} should be {}", weight, (weight + (middle - left)));
+        }
+    }
+
+    if left == middle && middle != right {
+        let weight = weights.get(children[2]).unwrap();
+        if right > middle {
+            panic!("{} should be {}", weight, (weight - (right - middle)));
+        } else {
+            panic!("{} should be {}", weight, (weight + (middle - right)));
+        }
+    }
+
+    if left != middle && left == right {
+        let weight = weights.get(children[1]).unwrap();
+        if middle > right {
+            panic!("{} should be {}", weight, (weight - (middle - right)));
+        } else {
+            panic!("{} should be {}", weight, (weight + (right - middle)));
+        }
+    }
+
+    weight + left + middle + right
 }
