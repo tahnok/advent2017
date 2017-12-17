1e87bdbee48b3c579ca6d258bb9f45a05e934c6b
Wesley Ellis <tahnok@gmail.com>
2017-12-17 09:19:23 -0500

    remove slow solution to day 17 part 2

--- rust/src/bin/day17_2.rs
+++ rust/src/bin/day17_2.rs
@@ -11,18 +11,17 @@ fn main() {
 fn next_value(input: &str) -> usize {
     let step: usize = input.trim().parse().unwrap();
 
-    let mut start = 0;
-    let mut buffer = vec![0];
-    for val in 1..50_000_000 {
-        start = (start + step) % (buffer.len()) + 1;
-        buffer.insert(start, val);
-        println!("{}", val);
-    }
-
-    for (index, &val) in buffer.iter().enumerate() {
-        if val == 0 {
-            return buffer[(index + val) % buffer.len()];
+    let mut zero_plus_loc = 1;
+    let mut zero_plus_val = 1;
+    let mut location = 1;
+    for val in 2..50_000_000 {
+        location = ((location + step) % val) + 1;
+        if location == zero_plus_loc {
+            zero_plus_val = val;
+        } else if location < zero_plus_loc {
+            zero_plus_loc += 1;
         }
     }
-    panic!("index of 0 not found");
+
+    zero_plus_val
 }
