0062f86b8a1d14ed5c97d81416f55217efef773a
Wesley Ellis <tahnok@gmail.com>
2018-01-02 08:01:59 -0500

    day 23 part 2

--- rust/src/bin/day23_2.rs
+++ rust/src/bin/day23_2.rs
@@ -1,10 +1,21 @@
+extern crate primal;
+
 fn main() {
     println!("{}", solve());
 }
 
-fn solve() -> isize {
+fn solve() -> usize {
     // see spreadsheet for work
     // but go from go from 106700 to 123700 stepping by 17
     // and check to see how many of those numbers have a * b = c
-    0
+    let mut a = 106700;
+    let mut found = 0;
+    while a < 123701 {
+        if !primal::is_prime(a) {
+            found += 1;
+        }
+
+        a += 17;
+    }
+    found 
 }
